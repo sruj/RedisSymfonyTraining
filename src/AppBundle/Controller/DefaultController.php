e4beaf06027639fb59687053dee2123caa0d4700
sruj <chryplewiczpawel@wp.pl>
2017-05-25 14:56:55 +0200

    SncRedisBundle instalacja, konfiguracja i hello world

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
+     * predis
+     *
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
@@ -29,6 +31,25 @@ class DefaultController extends Controller
 
         return $this->render('@App/index.html.twig', [
             'mama'=>$mam
+        ]);
+    }
+
+    /**
+     * snc redis bundle - predis w symfony
+     * https://github.com/snc/SncRedisBundle/blob/master/Resources/doc/index.md
+     *
+     * konfiguruje klienta w config.yml
+     * klientad 'default' tworzę zwracając serwis 'snc_redis.default'
+     *
+     * @Route("/snc", name="snc")
+     */
+    public function redisAction(Request $request)
+    {
+        $redis = $this->get('snc_redis.default');
+        $val = $redis->incr('foo:bar');
+
+        return $this->render('@App/index.html.twig', [
+            'mama'=>$val
         ]);
     }
 }
